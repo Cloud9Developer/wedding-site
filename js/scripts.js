512afb5b8f6daeab8cb6fed8fa213e94d17a818c
Christopher Hebert <chebert@ibm.com>
2026-07-15 15:54:39 -0500

    updated versions, cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -145,22 +145,18 @@ $(document).ready(function () {
     });
     /***************** Smooth Scrolling ******************/
 
-    $(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
-        $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top - 90
-                    }, 2000);
-                    return false;
-                }
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html').animate({
+                    scrollTop: target.offset().top - 90
+                }, 2000);
+                return false;
             }
-        });
-
+        }
     });
 
     /********************** Social Share buttons ***********************/
